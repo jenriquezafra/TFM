b5408c4917195981a07ba973c9f06a506be6699a
Enrique Zafra <kikekzm.98@gmail.com>
2025-12-20 11:03:07 +0100

    Refactor model and training configurations; add YAML loading in train.py

--- src/train.py
+++ src/train.py
@@ -5,3 +5,33 @@
 #   - early stopping, 
 #   - checkpoints, 
 #   - basic logging
+
+import yaml
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers, regularizers
+from scipy.stats import norm
+from pathlib import Path
+
+######################################## CONFIG LOAD ########################################
+PROJECT_PATH = Path("__file__").resolve().parent
+config_path = PROJECT_PATH / "configs" / "train.yaml"
+
+with open(config_path, "r") as f:
+    config = yaml.safe_load(f)
+
+
+CONFIG_COMPILER = config["compile"]
+
+optimizer = CONFIG_COMPILER["name"]
+optimizer_lr = CONFIG_COMPILER["learning_rate"]
+loss = config["loss"]["name"]
+metrics = config["metrics"]
+
+
+######################################## IMPORT THE MODEL ########################################
+
+
+######################################## MODEL TRAINING ########################################
+
